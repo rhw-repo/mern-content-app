1485efa8d7d4f6529f410f07af6c616bd83ec4b5
Ruth Westnidge <rwestnidge@gmail.com>
2023-02-13 15:39:04 +0000

    Changes const {id} to {_id}.Adds WIP TODO comment

--- frontend/src/pages/Read.js
+++ frontend/src/pages/Read.js
@@ -1,14 +1,18 @@
+// TODO: fetch individual article after <h2>
+
 import { useParams } from "react-router-dom"
+
+
 
 // constant named id is equal to built in react hook useParams
 // allows access to id of individual piece of content stored in database
 // placing within template allows output into this component 
 const Read = () => {
-    const { id } = useParams()
+    const { _id } = useParams()
 
     return ( 
         <div className="read">
-            <h2>Content Details - { id }</h2>
+            <h2>Content Details - { _id }</h2>
         </div>
      );
 }
