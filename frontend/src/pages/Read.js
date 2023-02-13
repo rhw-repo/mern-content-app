a54d450544c4036ff4a88e70ba36164d06a0ae09
Ruth Westnidge <rwestnidge@gmail.com>
2023-02-13 13:58:14 +0000

    Adds useParams hook, outputs id to template

--- frontend/src/pages/Read.js
+++ frontend/src/pages/Read.js
@@ -1,7 +1,14 @@
+import { useParams } from "react-router-dom"
+
+// constant named id is equal to built in react hook useParams
+// allows access to id of individual piece of content stored in database
+// placing within template allows output into this component 
 const Read = () => {
+    const { id } = useParams()
+
     return ( 
         <div className="read">
-            <h2>Content Details</h2>
+            <h2>Content Details - { id }</h2>
         </div>
      );
 }
