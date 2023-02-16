885f294f067059e9c0437356209ad47a0cff22d1
Ruth Westnidge <rwestnidge@gmail.com>
2023-02-16 15:27:24 +0000

    Deletes empty lines for readability

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,9 +2,6 @@ import { Link } from 'react-router-dom'
 import { useAuthContext } from '../hooks/useAuthContext';
 import { useLogout } from '../hooks/useLogout';
 
-
-
-
 const Navbar = () => {
 
     const { logout } = useLogout()
