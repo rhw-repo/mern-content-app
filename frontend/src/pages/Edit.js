68d61b7b173f9e0dc0b4e058e7b49e61f6ebd64a
Ruth Westnidge <rwestnidge@gmail.com>
2023-02-18 19:00:54 +0000

    Initial commit. WIP

--- frontend/src/pages/Edit.js
+++ frontend/src/pages/Edit.js
@@ -1,46 +1,70 @@
+///     TODO analyse + debug line 28 navigate component is not working
+
 import { useState, useEffect } from "react";
-import useFetch from "../hooks/useFetch";
+import { useAuthContext } from "../hooks/useAuthContext";
 
-function Edit({ materialId }) {
-    const [material, setMaterial] = useState("")
-   
+const Edit = ({ material, onUpdateComplete, navigate }) => {
+  const [title, setTitle] = useState(material.title);
+  const [body, setBody] = useState(material.body);
+  const [tags, setTags] = useState(material.tags);
+  const { user } = useAuthContext()
 
-    useEffect(() => {
-        useFetch("/api/materials/" + _id)
-       //fetch(`/api/materials/${materialId}`)
-            .then(response => response.json())
-            .then(material => setMaterial(material.title))
-            .then(material => setMaterial(material.body))
-            .catch(error => console.error(error))
-    }, [materialId])
+  useEffect(() => {
+    setTitle(material.title);
+    setBody(material.body);
+    setTags(material.tags);
+  }, [material]);
 
-    function handleContentChange(event) {
-        setMaterial(event.target.value)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const updatedMaterial = { title, body, tags };
+    const response = await fetch(`/api/materials/${material._id}`, {
+      method: "PATCH", // or PUT
+      headers: 
+      { "Content-Type": "application/json", 
+      "Authorization": `Bearer ${user.token}`},
+      body: JSON.stringify(updatedMaterial),
+    });
+    if (response.ok) {
+      onUpdateComplete();
+      navigate(`/materials/${material._id}`);
     }
+  };
 
-    function handleSaveClick() {
-        fetch(`api/materials/$d{materialId}`, {
-            method: "PUT",
-            headers: {
-                "Content type": "application/json"
-            },
-            body: JSON.stringify({
-                content: material
-            })
-        })
-            .then(response => response.json())
-            .then(material => {
-                console.log("Document saved", material)
-                // Add code to indicate success to the user 
-            })
-            .catch(error => console.error(error))
-    }
-return (
-    <div>
-        <h1>Edit Page</h1>
-        <textarea value={material} onChange={handleContentChange} />
-        <button onClick={handleSaveClick}>Save</button>
+  return (
+    <div className="edit">
+      <h3>Edit Material</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
-)
-}
+        <div>
+          <label htmlFor="body">Body:</label>
+          <textarea
+            id="body"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+        </div>
+        <div>
+          <label htmlFor="tags">Tags:</label>
+          <input
+            type="text"
+            id="tags"
+            value={tags}
+            onChange={(e) => setTags(e.target.value)}
+          />
+        </div>
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
+};
+
 export default Edit;
